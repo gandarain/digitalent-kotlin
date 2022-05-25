6256a7f96a91834bf34b72c913a72fe6cf6ab605
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-05-25 21:28:52 +0700

    destructuring declarations

--- chapter-1/data-classes-and-collections/data-class/src/main/kotlin/com/dicoding/kotlin/App.kt
+++ chapter-1/data-classes-and-collections/data-class/src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -30,4 +30,21 @@ fun main () {
     // copy data class
     val dataUser4 = dataUser3.copy(age = 18)
     println(dataUser4)
+
+    println()
+
+    // Destructuring Declarations
+    val dataUser5 = DataUser("Ganda", 25)
+    val nameUser = dataUser5.component1()
+    val ageUser = dataUser5.component2()
+    println("My name is $nameUser, I am $ageUser years old")
+
+    println()
+
+    val (name, age) = dataUser5
+    println("My name is $name, I am $age years old")
+
+    println()
+
+    dataUser5.intro()
 }
--- chapter-1/data-classes-and-collections/data-class/src/main/kotlin/com/dicoding/kotlin/DataClasses.kt
+++ chapter-1/data-classes-and-collections/data-class/src/main/kotlin/com/dicoding/kotlin/DataClasses.kt
@@ -24,4 +24,8 @@ class User(val name: String, val age: Int) {
     }
 }
 
-data class DataUser(val name: String, val age: Int)
+data class DataUser(val name: String, val age: Int) {
+    fun intro() {
+        println("My name is $name, I am $age years old")
+    }
+}
