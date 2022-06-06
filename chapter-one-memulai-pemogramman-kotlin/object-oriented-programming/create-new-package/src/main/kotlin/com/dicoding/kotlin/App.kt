c252e002c7b8ea873ef4b5104dadbd9a499d03ca
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-06-06 14:44:18 +0700

    oop - create new package

---
+++ chapter-one-memulai-pemogramman-kotlin/object-oriented-programming/create-new-package/src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -0,0 +1,11 @@
+package com.dicoding.kotlin
+
+import com.dicoding.kotlin.oop.utils.*
+
+fun main() {
+    sayHello()
+    println(pow(3.0, 2.0))
+    println(factorial(4.0))
+    println(PI)
+    println(areaOfCircle(13.0))
+}
