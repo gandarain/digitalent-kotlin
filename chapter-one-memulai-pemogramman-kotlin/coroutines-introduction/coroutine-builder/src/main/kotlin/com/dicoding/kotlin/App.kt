9fe8a3f48c574be2e04594af00d7bf32974e2c10
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-06-11 16:53:14 +0700

    coroutine - builder

---
+++ chapter-one-memulai-pemogramman-kotlin/coroutines-introduction/coroutine-builder/src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -0,0 +1,30 @@
+package com.dicoding.kotlin
+
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+fun main() = runBlocking {
+    val timeOne = measureTimeMillis {
+        val capital = getCapital()
+        val income = getIncome()
+        println("Your profit is ${income - capital}")
+    }
+
+    val timeTwo = measureTimeMillis {
+        val capital = async { getCapital() }
+        val income = async { getIncome() }
+        println("Your profit is ${income.await() - capital.await()}")
+    }
+
+    println("Completed in $timeOne vs $timeTwo")
+}
+
+suspend fun getCapital(): Int {
+    delay(1000L)
+    return 50000
+}
+
+suspend fun getIncome(): Int {
+    delay(1000L)
+    return 75000
+}
