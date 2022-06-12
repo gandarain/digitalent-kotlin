be29ff387cb8015be93ae14fc04c4735ba26d60f
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-06-12 16:37:13 +0700

    coroutine - deferred

---
+++ chapter-one-memulai-pemogramman-kotlin/coroutines-introduction/deferred/src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -0,0 +1,19 @@
+package com.dicoding.kotlin
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    val capital = async { getCapital() }
+    val income = async { getIncome() }
+    println("Your profit is ${income.await() - capital.await()}")
+}
+
+suspend fun getCapital(): Int {
+    delay(1000L)
+    return 50000
+}
+
+suspend fun getIncome(): Int {
+    delay(1000L)
+    return 75000
+}
