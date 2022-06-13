28a35e4f97d44b3e8b1ba79d1f53d0ca32fc8165
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-06-13 19:49:36 +0700

    coroutine - single thread context

---
+++ chapter-one-memulai-pemogramman-kotlin/coroutines-introduction/coroutine-thread/single-thread-context/src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -0,0 +1,12 @@
+package com.dicoding.kotlin
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking<Unit> {
+    val dispatcher = newSingleThreadContext("myThread")
+    launch(dispatcher) {
+        println("Starting in ${Thread.currentThread().name}")
+        delay(1000)
+        println("Resuming in ${Thread.currentThread().name}")
+    }.start()
+}
