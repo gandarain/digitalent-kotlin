3695f8879e0324aefb8b0ee5f75246e615f2a917
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-06-17 21:56:06 +0700

    intent - explicit intent

---
+++ chapter-two-fundamental-android/explicit-intent/app/src/main/java/com/example/explicitintent/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.explicitintent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+    }
+}
