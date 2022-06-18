ec9c2fe9ffb857f340bbaab555a3a467c1d14d23
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-06-18 12:54:40 +0700

    create error IndexOutOfBoundsException

--- chapter-two-fundamental-android/debugging-and-logging/app/src/main/java/com/example/debuggingandlogging/MainActivity.kt
+++ chapter-two-fundamental-android/debugging-and-logging/app/src/main/java/com/example/debuggingandlogging/MainActivity.kt
@@ -2,25 +2,41 @@ package com.example.debuggingandlogging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private var btnSetValue: Button? = null
+    private lateinit var btnSetValue: Button
     private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnSetValue = findViewById(R.id.btn_set_value)
         tvText = findViewById(R.id.tv_text)
-        btnSetValue!!.setOnClickListener(this)
+
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Narenda Wicaksono")
+        names.add("Kevin")
+        names.add("Yoza")
     }
 
     override fun onClick(v: View?) {
         if (v?.id == R.id.btn_set_value) {
-            tvText.text = "19"
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+
+            for (i in 0..3) {
+                name.append(names[i]).append("\n")
+            }
+
+            tvText.text = name
         }
     }
 }
