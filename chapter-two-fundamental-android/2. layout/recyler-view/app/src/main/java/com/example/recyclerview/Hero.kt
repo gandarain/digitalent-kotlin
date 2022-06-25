3d83e86cf21fcefdc128ece84f5830694dc23130
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-06-25 14:11:30 +0700

    create recycler view heroes

---
+++ chapter-two-fundamental-android/2. layout/recyler-view/app/src/main/java/com/example/recyclerview/Hero.kt
@@ -0,0 +1,11 @@
+package com.example.recyclerview
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: Int
+): Parcelable
---
+++ chapter-two-fundamental-android/2. layout/recyler-view/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private var list = ArrayList<Hero>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(listHeroes)
+        showRecyclerList()
+    }
+
+    private val listHeroes: ArrayList<Hero>
+        get() {
+            val dataName = resources.getStringArray(R.array.data_name)
+            val dataDescription = resources.getStringArray(R.array.data_description)
+            val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+            val listHero = ArrayList<Hero>()
+            for (i in dataName.indices) {
+                val hero = Hero(dataName[i],dataDescription[i], dataPhoto.getResourceId(i, -1))
+                listHero.add(hero)
+            }
+            return listHero
+        }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
+    }
+}
