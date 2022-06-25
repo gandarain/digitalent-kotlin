cefb9418ad05b02543359a21f2a2c77dd576e3a9
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-06-25 14:22:27 +0700

    show grid layout manager

--- chapter-two-fundamental-android/2. layout/recyler-view/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ chapter-two-fundamental-android/2. layout/recyler-view/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.recyclerview
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -35,7 +37,13 @@ class MainActivity : AppCompatActivity() {
         }
 
     private fun showRecyclerList() {
-        rvHeroes.layoutManager = LinearLayoutManager(this)
+        // rvHeroes.layoutManager = LinearLayoutManager(this)
+        // rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        if (applicationContext.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        } else {
+            rvHeroes.layoutManager = LinearLayoutManager(this)
+        }
         val listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
 
