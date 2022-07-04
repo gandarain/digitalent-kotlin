1a63c8e85f7ac3e13694f5806b5a0efe8ca24a81
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-07-04 22:22:30 +0700

    create tab layout

---
+++ chapter-two-fundamental-android/3. navigation/tab-layout/app/src/main/java/com/dicoding/tablayout/SectionsPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.dicoding.tablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when(position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = ProfileFragment()
+        }
+        return fragment as Fragment
+    }
+}
