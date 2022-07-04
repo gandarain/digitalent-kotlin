d8fe7461186d1ea9987af128e3a8de26178861e6
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-07-04 21:08:35 +0700

    search view on action bar

--- chapter-two-fundamental-android/3. navigation/option-menu-action-bar/app/src/main/java/com/example/optionmenuactionbar/MainActivity.kt
+++ chapter-two-fundamental-android/3. navigation/option-menu-action-bar/app/src/main/java/com/example/optionmenuactionbar/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.optionmenuactionbar
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import android.widget.Toast
 import com.example.optionmenuactionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,27 +20,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            // TODO Gunakan method ini ketika search selesai atau OK
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+
+            // TODO Gunakan method ini untuk merespon tiap perubahan huruf pada searchView
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        return when(item.itemId) {
             R.id.menu1 -> {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, MenuFragment())
                     .addToBackStack(null)
                     .commit()
-                return true
+                true
             }
             R.id.menu2 -> {
                 val i = Intent(this, MenuActivity::class.java)
                 startActivity(i)
-                return true
+                true
             }
-            else -> return true
+            else -> true
         }
     }
 }
