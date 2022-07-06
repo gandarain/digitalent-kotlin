b9532dea0a683bb9e273bb040c92db6e118e87e2
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-07-06 21:55:28 +0700

    implement Executors and Handlers

--- chapter-two-fundamental-android/4. background process & networking/background-thread/app/src/main/java/com/dicoding/backgroundthread/MainActivity.kt
+++ chapter-two-fundamental-android/4. background process & networking/background-thread/app/src/main/java/com/dicoding/backgroundthread/MainActivity.kt
@@ -2,8 +2,11 @@ package com.dicoding.backgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,20 +16,28 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btn_start)
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
         btnStart.setOnClickListener {
-            try {
-                // simulate process compressing
-                for (i in 0..10) {
-                    Thread.sleep(APP_FORCE_CLOSE)
-                    val percentage = i * 10
-                    if (percentage == 100) {
-                        tvStatus.setText(R.string.task_completed)
-                    } else {
-                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+            executor.execute {
+                try {
+                    // simulate process compressing
+                    for (i in 0..10) {
+                        Thread.sleep(APP_FREEZE)
+                        val percentage = i * 10
+                        handler.post {
+                            // update ui in main thread
+                            if (percentage == 100) {
+                                tvStatus.setText(R.string.task_completed)
+                            } else {
+                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
                     }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
                 }
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
             }
         }
     }
