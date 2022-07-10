d01b8d9705bc997e517e90ee73552c0163deebcf
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-07-10 18:45:43 +0700

    get review restaurant

---
+++ chapter-two-fundamental-android/4. background process & networking/retrofit/app/src/main/java/com/dicoding/retrofit/ApiService.kt
@@ -0,0 +1,11 @@
+package com.dicoding.retrofit
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+}
