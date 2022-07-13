6266c6cc633a3b8da15c3f376efeba88df810083
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-07-13 23:00:21 +0700

    subscribe live data

--- chapter-two-fundamental-android/5. android architecture component/live-data/app/src/main/java/com/dicoding/livedata/MainActivity.kt
+++ chapter-two-fundamental-android/5. android architecture component/live-data/app/src/main/java/com/dicoding/livedata/MainActivity.kt
@@ -2,10 +2,31 @@ package com.dicoding.livedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.livedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var mLiveDataTimerViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        mLiveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            activityMainBinding.timerTextview.text = newText
+        }
+
+        mLiveDataTimerViewModel.getElapsedTime().observe(this@MainActivity, elapsedTimeObserver)
     }
 }
