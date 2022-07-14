3704ba3a34f9a94ae7e2b92434f96496f494d800
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-07-14 20:08:36 +0700

    ### Fix line spacing

--- chapter-two-fundamental-android/5. android architecture component/live-data-api/app/src/main/java/com/dicoding/livedataapi/MainActivity.kt
+++ chapter-two-fundamental-android/5. android architecture component/live-data-api/app/src/main/java/com/dicoding/livedataapi/MainActivity.kt
@@ -13,7 +13,7 @@ import com.dicoding.livedataapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
