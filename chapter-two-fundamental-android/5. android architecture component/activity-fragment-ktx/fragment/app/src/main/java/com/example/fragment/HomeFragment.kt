e87b24ae3fb4047e054e7cfcbc67968859620a02
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-07-14 21:41:02 +0700

    ### Use Android KTX for Fragment

--- chapter-two-fundamental-android/5. android architecture component/activity-fragment-ktx/fragment/app/src/main/java/com/example/fragment/HomeFragment.kt
+++ chapter-two-fundamental-android/5. android architecture component/activity-fragment-ktx/fragment/app/src/main/java/com/example/fragment/HomeFragment.kt
@@ -6,13 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.commit
 
 class HomeFragment : Fragment(), View.OnClickListener {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
@@ -26,10 +26,9 @@ class HomeFragment : Fragment(), View.OnClickListener {
         if (v?.id == R.id.btn_category) {
             val mCategoryFragment = CategoryFragment()
             val mFragmentManager = parentFragmentManager
-            mFragmentManager.beginTransaction().apply {
-                replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
+            mFragmentManager.commit {
                 addToBackStack(null)
-                commit()
+                replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
             }
         }
     }
