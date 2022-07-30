3aa7ca5f2af6022f86371f33a8125d836c939a7c
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-07-30 14:52:43 +0700

    ### Setup DB

---
+++ chapter-two-fundamental-android/7. local data persistence/note-app-with-sqlite/app/src/main/java/com/dicoding/noteappwithsqlite/db/NoteHelper.kt
@@ -0,0 +1,74 @@
+package com.dicoding.noteappwithsqlite.db
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns._ID
+import com.dicoding.noteappwithsqlite.db.DatabaseContract.NoteColumns.Companion.TABLE_NAME
+
+class NoteHelper(context: Context) {
+
+    private var dataBaseHelper: DatabaseHelper = DatabaseHelper(context)
+    private lateinit var database: SQLiteDatabase
+
+    companion object {
+        private const val DATABASE_TABLE = TABLE_NAME
+
+        private var INSTANCE: NoteHelper? = null
+
+        fun getInstance(context: Context): NoteHelper =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: NoteHelper(context)
+            }
+    }
+
+    @Throws
+    fun open() {
+        database = dataBaseHelper.writableDatabase
+    }
+
+    fun close() {
+        dataBaseHelper.close()
+
+        if(database.isOpen) {
+            database.close()
+        }
+    }
+
+    fun queryAll(): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            null,
+            null,
+            null,
+            null,
+            "$_ID ASC"
+        )
+    }
+
+    fun queryById(id: String): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            "$_ID = ?",
+            arrayOf(id),
+            null,
+            null,
+            null,
+            null)
+    }
+
+    fun insert(values: ContentValues?): Long {
+        return database.insert(DATABASE_TABLE, null, values)
+    }
+
+    fun update(id: String, values: ContentValues?): Int {
+        return database.update(DATABASE_TABLE, values, "$_ID = ?", arrayOf(id))
+    }
+
+    fun deleteById(id: String): Int {
+        return database.delete(DATABASE_TABLE, "$_ID = '$id'", null)
+    }
+}
