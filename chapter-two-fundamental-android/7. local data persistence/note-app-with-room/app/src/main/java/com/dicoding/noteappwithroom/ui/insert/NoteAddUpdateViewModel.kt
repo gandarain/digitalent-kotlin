85c4c264c27194dc36a7ec71b4d2c5d10e96709c
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-07-30 22:37:10 +0700

    ### Update and Add Note Activity

---
+++ chapter-two-fundamental-android/7. local data persistence/note-app-with-room/app/src/main/java/com/dicoding/noteappwithroom/ui/insert/NoteAddUpdateViewModel.kt
@@ -0,0 +1,22 @@
+package com.dicoding.noteappwithroom.ui.insert
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import com.dicoding.noteappwithroom.database.Note
+import com.dicoding.noteappwithroom.repository.NoteRepository
+
+class NoteAddUpdateViewModel(application: Application) : ViewModel() {
+    private val mNoteRepository: NoteRepository = NoteRepository(application)
+
+    fun insert(note: Note) {
+        mNoteRepository.insert(note)
+    }
+
+    fun update(note: Note) {
+        mNoteRepository.update(note)
+    }
+
+    fun delete(note: Note) {
+        mNoteRepository.delete(note)
+    }
+}
