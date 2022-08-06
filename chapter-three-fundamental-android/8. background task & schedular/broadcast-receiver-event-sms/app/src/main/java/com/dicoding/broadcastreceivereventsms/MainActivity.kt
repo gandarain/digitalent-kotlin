acf77cd21b6ab00ea1ca9c0c8b1878a9052e8c99
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-08-06 20:43:10 +0700

    ### SMS Handler

--- chapter-three-fundamental-android/8. background task & schedular/broadcast-receiver-event-sms/app/src/main/java/com/dicoding/broadcastreceivereventsms/MainActivity.kt
+++ chapter-three-fundamental-android/8. background task & schedular/broadcast-receiver-event-sms/app/src/main/java/com/dicoding/broadcastreceivereventsms/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.dicoding.broadcastreceivereventsms
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.dicoding.broadcastreceivereventsms.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnPermission?.setOnClickListener(this)
+    }
+
+    var requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
---
+++ chapter-three-fundamental-android/8. background task & schedular/broadcast-receiver-event-sms/app/src/main/java/com/dicoding/broadcastreceivereventsms/SmsReceiver.kt
@@ -0,0 +1,53 @@
+package com.dicoding.broadcastreceivereventsms
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.telephony.SmsMessage
+import android.util.Log
+import java.lang.Exception
+
+class SmsReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val bundle = intent.extras
+
+        try {
+            if (bundle != null) {
+                /*
+                Bundle dengan key "pdus" sudah merupakan standar yang digunakan oleh system
+                 */
+                val pdusObj = bundle.get("pdus") as Array<*>
+                for (aPdusObj in pdusObj) {
+                    val currentSms = getIncomingMessage(aPdusObj as Any, bundle)
+                    val senderNum = currentSms.displayOriginatingAddress
+                    val message = currentSms.displayMessageBody
+                    Log.d(TAG, "senderNum: $senderNum; message: $message")
+                    val showSmsIntent = Intent(context, SmsReceiverActivity::class.java)
+                    showSmsIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_MESSAGE, message)
+                    showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_NO, senderNum)
+                    context.startActivity(showSmsIntent)
+                }
+            }
+        } catch (e: Exception) {
+            Log.d(TAG, "Exception smsReceiver $e")
+        }
+    }
+
+    private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage {
+        val currentSms: SmsMessage
+        val format = bundle.getString("format")
+        currentSms = if (Build.VERSION.SDK_INT >= 23) {
+            SmsMessage.createFromPdu(aObject as ByteArray, format)
+        } else SmsMessage.createFromPdu(aObject as ByteArray)
+
+        return currentSms
+    }
+
+    companion object {
+        private val TAG = SmsReceiver::class.java.simpleName
+    }
+}
