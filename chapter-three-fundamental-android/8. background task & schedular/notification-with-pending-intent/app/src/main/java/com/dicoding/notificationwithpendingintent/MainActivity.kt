b1874082abe37b2ca8860b0d32e871d8f325c10e
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-08-09 21:39:17 +0700

    ### Implement Pending Intent

--- chapter-three-fundamental-android/8. background task & schedular/notification-with-pending-intent/app/src/main/java/com/dicoding/notificationwithpendingintent/MainActivity.kt
+++ chapter-three-fundamental-android/8. background task & schedular/notification-with-pending-intent/app/src/main/java/com/dicoding/notificationwithpendingintent/MainActivity.kt
@@ -2,8 +2,11 @@ package com.dicoding.notificationwithpendingintent
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -25,8 +28,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun sendNotification() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0)
+
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentIntent(pendingIntent)
             .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
             .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_notifications_active_24))
             .setContentTitle(resources.getString(R.string.content_title))
