aa3e378ab578beaa3cddaba3dbda03a056a3ed29
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-08-10 21:36:00 +0700

    ### Implement Task Stack Builder

---
+++ chapter-three-fundamental-android/8. background task & schedular/task-and-back-stack-with-task-stack-builder/app/src/main/java/com/dicoding/taskandbackstackwithtaskstackbuilder/DetailActivity.kt
@@ -0,0 +1,27 @@
+package com.dicoding.taskandbackstackwithtaskstackbuilder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.taskandbackstackwithtaskstackbuilder.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val title = intent.getStringExtra(EXTRA_TITLE)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        binding.tvTitle.text = title
+        binding.tvMessage.text = message
+    }
+
+    companion object {
+        const val EXTRA_TITLE = "extra_title"
+        const val EXTRA_MESSAGE = "extra_message"
+    }
+}
