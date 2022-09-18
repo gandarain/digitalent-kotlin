5076a0beab44ec4532b4410916b3c36fe0c2a482
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-09-18 21:11:44 +0700

    ### Implement Stack View Widget

--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/1. advance-ui/1.3 android-widget/1.3.3 stackview-widget/app/src/main/java/com/dicoding/stackviewwidget/ImageBannerWidget.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/1. advance-ui/1.3 android-widget/1.3.3 stackview-widget/app/src/main/java/com/dicoding/stackviewwidget/ImageBannerWidget.kt
@@ -1,14 +1,47 @@
 package com.dicoding.stackviewwidget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
+import android.widget.Toast
+import androidx.core.net.toUri
 
 /**
  * Implementation of App Widget functionality.
  */
 class ImageBannerWidget : AppWidgetProvider() {
+    companion object {
+        private const val TOAST_ACTION = "com.dicoding.picodiploma.TOAST_ACTION"
+        const val EXTRA_ITEM = "com.dicoding.picodiploma.EXTRA_ITEM"
+
+        //pindahkan fungsi ini ke companion object, karena kita akan memanggil fungsi ini dari luar kelas
+        private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+            val intent = Intent(context, StackWidgetService::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = intent.toUri(Intent.URI_INTENT_SCHEME).toUri()
+
+            val views = RemoteViews(context.packageName, R.layout.image_banner_widget)
+            views.setRemoteAdapter(R.id.stack_view, intent)
+            views.setEmptyView(R.id.stack_view, R.id.empty_view)
+
+            val toastIntent = Intent(context, ImageBannerWidget::class.java)
+            toastIntent.action = TOAST_ACTION
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+
+            val toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+                else 0
+            )
+            views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -27,18 +60,15 @@ class ImageBannerWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-}
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.image_banner_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
 
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+        if (intent.action != null) {
+            if (intent.action == TOAST_ACTION) {
+                val viewIndex = intent.getIntExtra(EXTRA_ITEM, 0)
+                Toast.makeText(context, "Touched view $viewIndex", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
