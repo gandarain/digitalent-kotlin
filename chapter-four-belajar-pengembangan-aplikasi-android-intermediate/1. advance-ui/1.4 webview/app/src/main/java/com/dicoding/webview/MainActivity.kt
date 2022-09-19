54c673874d1d179ce6d7724dd669be57773831cd
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-09-19 17:40:43 +0700

    ### Implement WebView - Add WebView tag on activity main xml - Load Dicoding web on main activity

--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/1. advance-ui/1.4 webview/app/src/main/java/com/dicoding/webview/MainActivity.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/1. advance-ui/1.4 webview/app/src/main/java/com/dicoding/webview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.dicoding.webview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val DICODING_URL = "https://www.dicoding.com"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.loadUrl(DICODING_URL)
     }
 }
