37b0b92a9fdead2427e17315d523446049636d4c
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-09-28 19:50:22 +0700

    ### Navigation to Setting Language

--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/3. localization-and-accessibility/3.1 multi-language/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/3. localization-and-accessibility/3.1 multi-language/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.dicoding.picodiploma.productdetail
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.WindowInsets
 import android.view.WindowManager
+import com.dicoding.picodiploma.productdetail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setupView()
+        setupAction()
     }
 
     private fun setupView() {
@@ -25,5 +32,11 @@ class MainActivity : AppCompatActivity() {
             )
         }
         supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.settingImageView.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
     }
 }
