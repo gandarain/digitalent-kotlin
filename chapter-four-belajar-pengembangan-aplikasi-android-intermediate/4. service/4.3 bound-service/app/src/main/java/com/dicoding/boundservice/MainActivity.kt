c4a32417157d67e4cba3b6fb2c8a838a7704936b
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-03 20:36:04 +0700

    #### Start and Stop Bound Service - Start Bound Service via onBind - Stop Bound Service via onUnbind

--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/4. service/4.3 bound-service/app/src/main/java/com/dicoding/boundservice/MainActivity.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/4. service/4.3 bound-service/app/src/main/java/com/dicoding/boundservice/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.dicoding.boundservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import com.dicoding.boundservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var boundStatus = false
+    private lateinit var boundService: MyBoundService
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName) {
+            boundStatus = false
+        }
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val myBinder = service as MyBoundService.MyBinder
+            boundService = myBinder.getService
+            boundStatus = true
+            getNumberFromService()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +51,28 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnStopForegroundService.setOnClickListener {
             stopService(foregroundService)
+        }
+
+        val boundService = Intent(this@MainActivity, MyBoundService::class.java)
+        binding.btnStartBoundService.setOnClickListener {
+            bindService(boundService, connection, BIND_AUTO_CREATE)
+        }
+        binding.btnStopBoundService.setOnClickListener {
+            unbindService(connection)
+        }
+    }
+
+    private fun getNumberFromService() {
+        boundService.numberLiveData.observe(this) { number ->
+            binding.tvBoundServiceNumber.text = number.toString()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (boundStatus) {
+            unbindService(connection)
+            boundStatus = false
         }
     }
 }
--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/4. service/4.3 bound-service/app/src/main/java/com/dicoding/boundservice/MyBoundService.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/4. service/4.3 bound-service/app/src/main/java/com/dicoding/boundservice/MyBoundService.kt
@@ -2,11 +2,56 @@ package com.dicoding.boundservice
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.*
 
 class MyBoundService : Service() {
 
+    companion object {
+        private val TAG = MyBoundService::class.java.simpleName
+    }
+
+    private var binder = MyBinder()
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+    val numberLiveData: MutableLiveData<Int> = MutableLiveData()
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        Log.d(TAG, "onBind: ")
+
+        serviceScope.launch {
+            for (i in 1..50) {
+                delay(1000)
+                Log.d(TAG, "Do Something $i")
+                numberLiveData.postValue(i)
+            }
+
+            Log.d(TAG, "Service Stopped")
+        }
+
+        return binder
+    }
+
+    internal inner class MyBinder: Binder() {
+        val getService: MyBoundService = this@MyBoundService
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.d(TAG, "onUnbind: ")
+        serviceJob.cancel()
+        return super.onUnbind(intent)
+    }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+        Log.d(TAG, "onRebind: ")
     }
 }
