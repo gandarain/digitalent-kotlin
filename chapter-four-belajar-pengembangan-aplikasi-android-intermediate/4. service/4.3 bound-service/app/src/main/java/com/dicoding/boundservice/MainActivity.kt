134a0adbb90d7fc69241529af5b764618c271ff0
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-03 20:22:51 +0700

    #### Init Bound Service Project

---
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/4. service/4.3 bound-service/app/src/main/java/com/dicoding/boundservice/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.dicoding.boundservice
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.boundservice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val backgroundService = Intent(this@MainActivity, MyBackgroundService::class.java)
+        binding.btnStartBackgroundService.setOnClickListener {
+            startService(backgroundService)
+        }
+        binding.btnStopBackgroundService.setOnClickListener {
+            stopService(backgroundService)
+        }
+
+        val foregroundService = Intent(this@MainActivity, MyForegroundService::class.java)
+        binding.btnStartForegroundService.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(foregroundService)
+            } else {
+                startService(foregroundService)
+            }
+        }
+        binding.btnStopForegroundService.setOnClickListener {
+            stopService(foregroundService)
+        }
+    }
+}
