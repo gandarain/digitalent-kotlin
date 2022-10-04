898e5ba8cef56a4eb1111fd95a426cf1484fbf05
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-04 20:53:17 +0700

    #### Setup Project based on Media Player Project

--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/5. media/5.1 media/5.1.3 media-player-with-service-and-notification/app/src/main/java/com/dicoding/mediaplayerwithserviceandnotification/MainActivity.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/5. media/5.1 media/5.1.3 media-player-with-service-and-notification/app/src/main/java/com/dicoding/mediaplayerwithserviceandnotification/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.dicoding.mediaplayerwithserviceandnotification
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private var mMediaPlayer: MediaPlayer? = null
+    private var isReady: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnPlay = findViewById<Button>(R.id.btn_play)
+        val btnStop = findViewById<Button>(R.id.btn_stop)
+
+        btnPlay.setOnClickListener {
+            if (!isReady) {
+                mMediaPlayer?.prepareAsync()
+            } else {
+                if (mMediaPlayer?.isPlaying as Boolean) {
+                    mMediaPlayer?.pause()
+                } else {
+                    mMediaPlayer?.start()
+                }
+            }
+        }
+
+        btnStop.setOnClickListener {
+            if (mMediaPlayer?.isPlaying as Boolean) {
+                mMediaPlayer?.stop()
+                isReady = false
+            }
+        }
+
+        init()
+    }
+
+    private fun init() {
+        mMediaPlayer = MediaPlayer()
+        val attribute = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+        mMediaPlayer?.setAudioAttributes(attribute)
+        val afd = applicationContext.resources.openRawResourceFd(R.raw.guitar_background)
+
+        try {
+            mMediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        mMediaPlayer?.setOnPreparedListener {
+            isReady = true
+            mMediaPlayer?.start()
+        }
+        mMediaPlayer?.setOnErrorListener { _, _, _ -> false }
     }
 }
