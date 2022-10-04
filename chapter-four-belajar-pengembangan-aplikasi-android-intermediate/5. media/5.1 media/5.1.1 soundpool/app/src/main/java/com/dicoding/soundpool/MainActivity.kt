1da2a4e5683f357a527166394fc50302231b7887
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-04 20:03:00 +0700

    #### Implement Sound Pool - SoundPool Builder - Load Sound

--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/5. media/5.1 media/5.1.1 soundpool/app/src/main/java/com/dicoding/soundpool/MainActivity.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/5. media/5.1 media/5.1.1 soundpool/app/src/main/java/com/dicoding/soundpool/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.dicoding.soundpool
 
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sp: SoundPool
+    private var soundId: Int = 0
+    private var spLoaded: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSound = findViewById<Button>(R.id.btn_sound_pool)
+
+        sp = SoundPool.Builder()
+            .setMaxStreams(10)
+            .build()
+
+        sp.setOnLoadCompleteListener { _, _, status ->
+            if (status == 0) {
+                spLoaded = true
+            } else {
+                Toast.makeText(this@MainActivity, "Gagal Load", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        soundId = sp.load(this, R.raw.clinking_glasses, 1)
+
+        btnSound.setOnClickListener {
+            if (spLoaded) {
+                sp.play(soundId, 1F, 1F, 0, 0, 1F)
+            }
+        }
     }
 }
