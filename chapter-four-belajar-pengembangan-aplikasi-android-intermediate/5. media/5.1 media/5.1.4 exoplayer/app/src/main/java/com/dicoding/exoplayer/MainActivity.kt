ec5ae93ff76512ee0d4a1bdf2d1f615d2eb2dd83
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-05 20:02:29 +0700

    #### Added audio into ExoPlayer

--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/5. media/5.1 media/5.1.4 exoplayer/app/src/main/java/com/dicoding/exoplayer/MainActivity.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/5. media/5.1 media/5.1.4 exoplayer/app/src/main/java/com/dicoding/exoplayer/MainActivity.kt
@@ -9,11 +9,13 @@ import com.google.android.exoplayer2.MediaItem
 class MainActivity : AppCompatActivity() {
     companion object {
         const val URL_VIDEO_DICODING = "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+        const val URL_AUDIO = "https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3"
     }
 
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
@@ -22,7 +24,9 @@ class MainActivity : AppCompatActivity() {
         viewBinding.videoView.player = player
 
         val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        val anotherMediaItem = MediaItem.fromUri(URL_AUDIO)
         player.setMediaItem(mediaItem)
+        player.addMediaItem(anotherMediaItem)
         player.prepare()
     }
 }
