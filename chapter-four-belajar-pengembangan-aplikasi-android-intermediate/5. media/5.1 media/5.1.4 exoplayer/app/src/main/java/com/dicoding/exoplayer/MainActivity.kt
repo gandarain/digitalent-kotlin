e9168649cd54f764d5767e1a486ee49c52ecf06c
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-05 19:55:50 +0700

    #### Showing video with ExoPlayer

--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/5. media/5.1 media/5.1.4 exoplayer/app/src/main/java/com/dicoding/exoplayer/MainActivity.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/5. media/5.1 media/5.1.4 exoplayer/app/src/main/java/com/dicoding/exoplayer/MainActivity.kt
@@ -2,10 +2,27 @@ package com.dicoding.exoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.exoplayer.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val URL_VIDEO_DICODING = "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+    }
+
+    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
+
+        val player = ExoPlayer.Builder(this@MainActivity).build()
+        viewBinding.videoView.player = player
+
+        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        player.setMediaItem(mediaItem)
+        player.prepare()
     }
 }
