1d5482129d90e2d34fa263f90379148c072bf21f
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-10 20:37:04 +0700

    #### Preparing Files of Individual Result Intents

--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/5. media/5.5 upload-file/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/5. media/5.5 upload-file/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -14,10 +14,16 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.dicoding.picodiploma.mycamera.databinding.ActivityMainBinding
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var getFile: File? = null
 
     companion object {
         const val CAMERA_X_RESULT = 200
@@ -96,7 +102,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun uploadImage() {
-        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+        if (getFile != null) {
+            val file = getFile as File
+
+            val description = "Ini adalah deksripsi gambar".toRequestBody("text/plain".toMediaType())
+            val requestImageFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
+                "photo",
+                file.name,
+                requestImageFile
+            )
+        } else {
+            Toast.makeText(this@MainActivity, "Silahkan masukkan berkas gambar terlebih dahulu", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private val launcherIntentCameraX = registerForActivityResult(
@@ -106,6 +124,7 @@ class MainActivity : AppCompatActivity() {
             val myFile = it.data?.getSerializableExtra("picture") as File
             val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
 
+            getFile = myFile
             val result = BitmapFactory.decodeFile(myFile.path)
 
             binding.previewImageView.setImageBitmap(result)
@@ -119,7 +138,7 @@ class MainActivity : AppCompatActivity() {
     ) {
         if (it.resultCode == RESULT_OK) {
             val myFile = File(currentPhotoPath)
-
+            getFile = myFile
             val result = BitmapFactory.decodeFile(myFile.path)
 
             binding.previewImageView.setImageBitmap(result)
@@ -132,6 +151,7 @@ class MainActivity : AppCompatActivity() {
         if (result.resultCode == RESULT_OK) {
             val selectedImg: Uri = result.data?.data as Uri
             val myFile = uriToFile(selectedImg, this@MainActivity)
+            getFile = myFile
             binding.previewImageView.setImageURI(selectedImg)
         }
     }
