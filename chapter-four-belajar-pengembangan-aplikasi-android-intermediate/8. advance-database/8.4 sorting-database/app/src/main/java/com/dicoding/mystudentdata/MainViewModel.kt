04d85f038fd0afae0047d1b27a22e1c9d37dca60
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-11-01 21:50:54 +0700

    #### added RawQuery to the DAO

--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/8. advance-database/8.4 sorting-database/app/src/main/java/com/dicoding/mystudentdata/MainViewModel.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/8. advance-database/8.4 sorting-database/app/src/main/java/com/dicoding/mystudentdata/MainViewModel.kt
@@ -1,18 +1,27 @@
 package com.dicoding.mystudentdata
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.dicoding.mystudentdata.database.Student
 import com.dicoding.mystudentdata.database.StudentAndUniversity
 import com.dicoding.mystudentdata.database.StudentWithCourse
 import com.dicoding.mystudentdata.database.UniversityAndStudent
+import com.dicoding.mystudentdata.helper.SortType
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val studentRepository: StudentRepository) : ViewModel() {
+    private val _sort = MutableLiveData<SortType>()
 
-    fun getAllStudent(): LiveData<List<Student>> = studentRepository.getAllStudent()
+    init {
+        _sort.value = SortType.ASCENDING
+    }
+
+    fun changeSortType(sortType: SortType) {
+        _sort.value = sortType
+    }
+
+    fun getAllStudent(): LiveData<List<Student>> = _sort.switchMap {
+        studentRepository.getAllStudent(it)
+    }
 
     fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentRepository.getAllStudentAndUniversity()
 
