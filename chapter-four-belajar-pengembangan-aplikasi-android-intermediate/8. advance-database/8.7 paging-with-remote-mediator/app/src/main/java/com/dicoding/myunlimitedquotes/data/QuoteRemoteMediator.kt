7822901914056aa38490172529247c478b2ae41c
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-11-05 14:12:07 +0700

    #### Creating a Pager in the Repository

---
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/8. advance-database/8.7 paging-with-remote-mediator/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRemoteMediator.kt
@@ -0,0 +1,51 @@
+package com.dicoding.myunlimitedquotes.data
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
+import com.dicoding.myunlimitedquotes.database.QuoteDatabase
+import com.dicoding.myunlimitedquotes.network.ApiService
+import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
+import java.lang.Exception
+
+@OptIn(ExperimentalPagingApi::class)
+class QuoteRemoteMediator(
+    private val database: QuoteDatabase,
+    private val apiService: ApiService
+): RemoteMediator<Int, QuoteResponseItem>() {
+
+    private companion object {
+        const val INITIAL_PAGE_INDEX = 1
+    }
+
+    override suspend fun initialize(): InitializeAction {
+        return InitializeAction.LAUNCH_INITIAL_REFRESH
+    }
+
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, QuoteResponseItem>
+    ): MediatorResult {
+        val page = INITIAL_PAGE_INDEX
+
+        try {
+            val responseData = apiService.getQuote(page, state.config.pageSize)
+
+            val endOfPaginationReached = responseData.isEmpty()
+
+            database.withTransaction {
+                if (loadType == LoadType.REFRESH) {
+                    database.quoteDao().deleteAll()
+                }
+                database.quoteDao().insertQuote(responseData)
+            }
+
+            return MediatorResult.Success(endOfPaginationReached = endOfPaginationReached)
+        } catch (exception: Exception) {
+            return MediatorResult.Error(exception)
+        }
+    }
+
+}
--- chapter-four-belajar-pengembangan-aplikasi-android-intermediate/8. advance-database/8.7 paging-with-remote-mediator/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRepository.kt
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/8. advance-database/8.7 paging-with-remote-mediator/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRepository.kt
@@ -1,10 +1,7 @@
 package com.dicoding.myunlimitedquotes.data
 
 import androidx.lifecycle.LiveData
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.liveData
+import androidx.paging.*
 import com.dicoding.myunlimitedquotes.adapter.QuotePagingSource
 import com.dicoding.myunlimitedquotes.database.QuoteDatabase
 import com.dicoding.myunlimitedquotes.network.ApiService
@@ -12,12 +9,14 @@ import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
 
 class QuoteRepository(private val quoteDatabase: QuoteDatabase, private val apiService: ApiService) {
     fun getQuote(): LiveData<PagingData<QuoteResponseItem>> {
+        @OptIn(ExperimentalPagingApi::class)
         return Pager(
             config = PagingConfig(
                 pageSize = 5
             ),
+            remoteMediator = QuoteRemoteMediator(quoteDatabase, apiService),,
             pagingSourceFactory = {
-                QuotePagingSource(apiService)
+                quoteDatabase.quoteDao().getAllQuote()
             }
         ).liveData
     }
