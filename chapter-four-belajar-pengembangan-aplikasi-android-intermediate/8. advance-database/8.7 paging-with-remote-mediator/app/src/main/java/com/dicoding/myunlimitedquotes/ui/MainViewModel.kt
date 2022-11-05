46554f0655517b7916d2b60c0d4fda0bd14de078
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-11-05 13:57:59 +0700

    #### Init Remote Mediator Project

---
+++ chapter-four-belajar-pengembangan-aplikasi-android-intermediate/8. advance-database/8.7 paging-with-remote-mediator/app/src/main/java/com/dicoding/myunlimitedquotes/ui/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.dicoding.myunlimitedquotes.ui
+
+import android.content.Context
+import androidx.lifecycle.*
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.dicoding.myunlimitedquotes.data.QuoteRepository
+import com.dicoding.myunlimitedquotes.di.Injection
+import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
+
+class MainViewModel(private val quoteRepository: QuoteRepository) : ViewModel() {
+    val quote: LiveData<PagingData<QuoteResponseItem>> =
+        quoteRepository.getQuote().cachedIn(viewModelScope)
+}
+
+class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(Injection.provideRepository(context)) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
